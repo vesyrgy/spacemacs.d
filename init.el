9705b2da90b2a195e949c36ae2d240b9a4723092
Lars Ysla <yslars@gmail.com>
2021-05-22 19:40:23 +0200

    revert removed code, since it seems to work now

--- init.el
+++ init.el
@@ -1347,4 +1347,54 @@ before packages are loaded."
 
 
 
-
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(after-save-hook
+   '(rmail-after-save-hook doom-modeline-update-vcs-text doom-modeline-update-vcs-icon doom-modeline-update-buffer-file-name org-roam-db-build-cache org-roam-buffer-update))
+ '(auto-save-default nil)
+ '(auto-save-visited-interval 1)
+ '(auto-save-visited-mode t)
+ '(deft-auto-save-interval 0.0)
+ '(deft-directory "~/org-roam")
+ '(org-agenda-files
+   '("~/org-roam/" "~/Documents/OrgMode/" "~/org-roam/daily/" "~/org-roam/wantto/"))
+ '(org-babel-load-languages
+   '((shell . t)
+     (js . t)
+     (dot . t)
+     (clojure . t)
+     (emacs-lisp . t)
+     (python . t)
+     (haskell . t)
+     (css . t)
+     (latex . t)))
+ '(org-directory "~/org-roam/")
+ '(org-refile-allow-creating-parent-nodes 'confirm)
+ '(org-refile-targets
+   '((org-agenda-files :tag . "refile_target")
+     (org-agenda-files :todo . "FAILED")))
+ '(org-refile-use-outline-path t)
+ '(org-roam-capture-after-find-file-hook '(org-roam-db-build-cache org-roam-buffer-update))
+ '(org-roam-db-update-method 'immediate)
+ '(org-todo-keywords
+   '((sequence "TODO" "DOING" "BLOCKED" "REVIEW" "|" "DONE" "ARCHIVED")
+     (sequence "WANTTO(w)" "SPEC" "FINDOUT" "DECIDE" "HOWTO" "TRY" "OBSERVE" "FAILED" "GOTO" "DO")
+     (sequence "STORY(s)" "TASK" "BUG" "|" "FIXED" "DONE" "WONTDO")
+     (sequence "QUESTION(q)" "|" "ANSWER" "REFERENCE")))
+ '(package-selected-packages
+   '(zetteldeft org-roam emacsql-sqlite3 flycheck-clj-kondo deft yasnippet-snippets yaml-mode xterm-color ws-butler writeroom-mode winum which-key web-mode web-beautify vterm volatile-highlights vmd-mode vi-tilde-fringe valign uuidgen use-package unkillable-scratch unicode-fonts undo-tree treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil toc-org terminal-here tagedit symon symbol-overlay string-inflection spaceline-all-the-icons smeargle slim-mode shell-pop scss-mode sass-mode restart-emacs ranger rainbow-mode rainbow-identifiers rainbow-delimiters pug-mode prettier-js popwin persistent-scratch pcre2el password-generator paradox ox-twbs ox-gfm overseer orgit org-superstar org-rich-yank org-re-reveal org-projectile org-present org-pomodoro org-mime org-journal org-download org-cliplink org-brain open-junk-file nodejs-repl nameless multi-term move-text mmm-mode markdown-toc magit-svn magit-section magit-gitflow macrostep lsp-ui lsp-treemacs lsp-origami lorem-ipsum livid-mode link-hint ligature keycast kaolin-themes json-navigator json-mode js2-refactor js-doc indent-guide impatient-mode hybrid-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-org-rifle helm-org helm-mode-manager helm-make helm-lsp helm-ls-git helm-gitignore helm-git-grep helm-flx helm-descbinds helm-css-scss helm-company helm-cider helm-c-yasnippet helm-ag grip-mode graphviz-dot-mode google-translate golden-ratio gnuplot gitignore-templates github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md fuzzy forge font-lock+ flyspell-correct-helm flycheck-pos-tip flycheck-package flycheck-elsa flx-ido fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-vimish-fold evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-easymotion evil-cleverparens evil-args evil-anzu eshell-z eshell-prompt-extras esh-help emr emojify emoji-cheat-sheet-plus emmet-mode elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-themes doom-modeline dired-quick-sort diminish diff-hl devdocs define-word csv-mode company-web company-statistics company-quickhelp company-emoji command-log-mode column-enforce-mode color-identifiers-mode clojure-snippets clean-aindent-mode cider-eval-sexp-fu centered-cursor-mode browse-at-remote auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adoc-mode ace-link ace-jump-helm-line ac-ispell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
